a0e815f1d1e08c064b9f9925de6faf78a838b696
royi <hellowol@naver.com>
2022-11-14 09:45:17 +0900

    🔨 add this +

--- memo.js
+++ memo.js
@@ -813,3 +813,18 @@ console.log(item.substring(2, 10));
 
 
 console.log('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ');
+
+function Prefixer(prefix) {
+	this.prefix = prefix;
+}
+
+Prefixer.prototype.prefixArray = function (arr) {
+	// this는 상위 스코프인 prefixArray 메소드 내의 this를 가리킨다.
+	return arr.map(x => `${this.prefix}  ${x}`);
+};
+
+const pre = new Prefixer('Hi');
+console.log(pre.prefixArray(['Lee', 'Kim']));
+
+console.log('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ');
+
