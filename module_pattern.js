c813225321914ce3457cc39929f51e7b80968700
Royi Kim <hellowol@naver.com>
2024-03-07 23:58:36 +0900

    update module pattern

--- module_pattern.js
+++ module_pattern.js
@@ -23,14 +23,32 @@ var module2 = {
     }
 }
 
+var module3 = (function() {
+    var util = {
+        init: function() {
+            console.log("util.init");
+        },
+
+        init2: function() {
+            console.log('util.init2');
+        }
+    };
+
+    return {
+        init: util.init
+    };
+})(); // IIFE를 추가하여 모듈 실행
+
 function outFunction() {
     console.log('module_pattern:13 outFunction', outFunction);
 }
 
-module.init();
-// module._privateFunction(); //불가
-console.log('module_pattern:32 module.privateValue', module.privateValue);
+// module.init();
+// // module._privateFunction(); //불가
+// console.log('module_pattern:32 module.privateValue', module.privateValue);
 
 
-module2.init();
-console.log('module_pattern:36 module2.publicValue', module2.publicValue);
+// module2.init();
+// console.log('module_pattern:36 module2.publicValue', module2.publicValue);
+
+module3.init();
