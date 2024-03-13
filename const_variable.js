e7b2823559eaf11ed60b85dc7d1c284bad38e4a9
kry <hellowol941226@gmail.com>
2024-03-13 16:27:34 +0900

    ✨ add Consts define

---
+++ const_variable.js
@@ -0,0 +1,24 @@
+var CONSTS = {
+    TABLE: {
+        BGCOLOR: {
+            EMPTY: '#FFF4E9',
+            NEW: '#ECF2FF',
+        },
+        COLOR: {
+            BLUE: '#1089FB'
+        }
+    },
+
+    COLOR: {
+        RED: '#F42D16',
+        PURPLE: '#A822C8',
+        GREEN: '#32A13C',
+        BLUE: '#1089FB',
+        GRAY: '#999'
+    },
+
+    FILE: {
+        MAX_SIZE: 10485760, //최대 허용 파일 크기(10MB)
+        DEFAULT_PATH_CODE: 'D'
+    }
+}
